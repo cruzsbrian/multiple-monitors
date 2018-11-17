8b9f46c3ea5b5fb612aa87b145e989095c8670b4
Brian Cruz <cruz.s.brian@gmail.com>
2018-11-17 00:00:42 -0800

    Initial commit. Geometry works

---
+++ monitors.py
@@ -0,0 +1,63 @@
+import subprocess
+import re
+
+operatorToArg = {
+        "=": "--same-as",
+        ">": "--right-of",
+        "<": "--left-of",
+        "^": "--above",
+        "V": "--below",
+        }
+
+oppositeOperator = {
+        "=": "--same-as",
+        ">": "--left-of",
+        "<": "--right-of",
+        "^": "--below",
+        "V": "--above",
+        }
+
+# get xrandr output as list of lines
+xrandrList = subprocess.check_output(["xrandr"]).decode("utf-8").split('\n')
+
+# filter into a list of monitor names (every line that contains ' connected')
+monitors = [
+        line.split(' ')[0]
+        for line in xrandrList
+        if " connected" in line
+        ]
+
+# for quick testing
+#monitors = ['M0', 'M1', 'M2', 'M3']
+
+# print a numbered list of monitors
+for i in range(len(monitors)):
+    print(i, monitors[i])
+
+config = input()
+
+cmd = ["xrandr"]
+
+enabled = []
+
+# find all pairs of monitor-operator-monitor (number, operator, number)
+pairs = re.findall(r'(?=(\d+[=<>^V]\d+))', config)
+
+for p in pairs:
+    # get the two relevant monitors and the operator
+    m1, m2 = [monitors[int(m)] for m in re.split('[=<>^V]', p)]
+    op = re.search('[=<>^V]', p).group()
+
+    if m1 not in enabled:
+        cmd += ["--output", m1, "--auto"]
+        enabled.append(m1)
+
+        if m2 in enabled:
+            cmd += [oppositeOperator.get(op), m2]
+
+    if m2 not in enabled:
+        cmd += ["--output", m2, "--auto"]
+        cmd += [operatorToArg.get(op), m1]
+        enabled.append(m2)
+
+print(' '.join(cmd))
